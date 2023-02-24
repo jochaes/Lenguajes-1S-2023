94929479366ea578992876d6357030626f76e99e
Josué Chaves Araya <d4n11083@estudiantec.cr>
2023-02-24 10:30:06 -0600

    Ejercicio 2 - Imperativo

--- Imperativo/Ejercicio_2/Ejercicio_2.go
+++ Imperativo/Ejercicio_2/Ejercicio_2.go
@@ -13,10 +13,61 @@ Ejercicio 1 de Lenguages Imperativos
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func draw(centerLine int){
+
+	middle := centerLine/2
+	ast := -1
+
+	for i := middle; i > (middle*-1)-1; i-- {
+
+		str := ""
+		spaces := i
+
+		if spaces < 0 {
+			spaces *=-1	
+			ast -= 2 
+		}else{
+			ast+=2
+		}
+
+		for j := 0; j < spaces; j++ {
+			str +="  "
+		}
+
+		for j := 0; j < ast; j++ {
+			str += "* "
+		}
+
+		fmt.Println(str)
+	}
+
+}
 
 
 func main(){
 
-	fmt.Println("Ejercicio 2")
+	centerLineElemStr := os.Args[1]
+	
+
+	//Convert command line argument to Integer
+	centerLineElem, err := strconv.Atoi(centerLineElemStr)
+	if err != nil {
+		fmt.Println("Error during conversion")
+		return
+	}
+
+	if centerLineElem <=0 || centerLineElem%2 != 1 {
+		fmt.Println("Input must be an odd number greater than 0")
+		return
+	}
+
+	fmt.Println("Drawing figure with center line of:",centerLineElem)
+	draw(centerLineElem)
+
 }
--- Imperativo/Ejercicio_2/readme.txt
+++ Imperativo/Ejercicio_2/readme.txt
@@ -12,5 +12,13 @@ Ejercicio 2 de Lenguages Imperativos
 
 Instrucciones para ejecutar el programa: 
 
-- Ejecutar el programa desde consola con el siguiente comando 
-$ go run Ejercicio_2.go
+
+
+- El programa está diseñado para aceptar input de la linea de comandos al momento de ejecutar el código. 
+- El input es la cantidad de "*" de la linea central 
+
+- Para ejecutar el programa con la linea de 5 "*" utilice el siguiente comando 
+$ go run Ejercicio_2.go 5
+
+
+
