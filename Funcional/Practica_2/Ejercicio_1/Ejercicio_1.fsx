e564ccb048e1fc75757891ad3853a9737d9b879c
Josué Chaves Araya <d4n11083@estudiantec.cr>
2023-04-04 17:31:14 -0600

    Ejercicio 1, practica 2 completado

--- Funcional/Practica_2/Ejercicio_1/Ejercicio_1.fsx
+++ Funcional/Practica_2/Ejercicio_1/Ejercicio_1.fsx
@@ -13,12 +13,71 @@ Ejercicio 1 de Lenguages Funcionales - Practica 2
 *)
 
 module Ejercicio_1 =
-  //funcion que imprima "hola mundo"
-  let holaMundo () = printfn "Hola Mundo"
-  
 
+  let grafo = [
+    [('i', 0);('a', 1);('b', 2)];
+    [('a', 1);('i', 0);('c', 3);('d', 2)];
+    [('b', 2);('i', 0);('c', 1);('d', 4)];
+    [('c', 3);('a', 1);('b', 1);('x', 2)];
+    [('d', 2);('a', 1);('b', 4);('f', 3)];
+    [('f', 3);('d', 2)];
+    [('x', 2);('c', 3)]
+]
+
+let miembro e (lista: ('a * 'b) list) =
+    lista
+    |> List.map (fun (x,_) -> x = e)
+    |> List.reduce (fun x y -> x || y)
+
+let rec vecinos nodo grafo =
+    match grafo with
+    | [] -> []
+    | ((head, peso)::adyacencias)::tail when nodo = head -> adyacencias
+    | _::tail -> vecinos nodo tail 
+
+
+let extender (ruta: _ list) grafo =
+    (vecinos (fst ruta.Head) grafo)
+    |> List.map (fun (x,peso) -> if (miembro x ruta) then [] else (x,peso)::ruta )
+    |> List.filter (fun x -> x <> [])
+
+let rec prof_aux fin (rutas: _ list list) grafo =
+    if rutas = [] then
+        []
+    elif fin = (fst rutas.Head.Head) then
+        //List.rev rutas.Head
+        List.append
+            ([List.rev rutas.Head])
+            (prof_aux fin rutas.Tail grafo)
+    else
+        prof_aux fin (List.append (extender rutas.Head grafo) rutas.Tail) grafo
+        
+let prof ini fin grafo =
+    prof_aux fin [[(ini,0)]] grafo
+    
+
+
+//**********************************************************************
+(*
+   Funciones que corresponden al ejercicio 2
+*)
+
+//Calcula el peso total de una ruta
+let rec pesoRuta (lista: ('a * int) list) =
+    match lista with
+    | [] -> 0
+    | (x,y)::tail -> y + (pesoRuta tail)
+
+//Da la ruta con menor peso
+let menorRuta (lista: ('a * int) list list) =
+    lista
+    |> List.reduce (fun x y -> if pesoRuta x < pesoRuta y then x else y)
 
 
 open Ejercicio_1
-holaMundo()
 
+printf "Grafo: \n%A" grafo
+printfn "\n"
+printfn "Rutas ( i->f ): \n%A" (prof 'i' 'f' grafo)
+printfn "\n"
+printf "Menor Ruta ( i->f ):  %A" (menorRuta (prof 'i' 'f' grafo))
--- Funcional/Practica_2/Ejercicio_1/readme.txt
+++ Funcional/Practica_2/Ejercicio_1/readme.txt
@@ -15,10 +15,13 @@ IDE: VS Code
 Plugin: Ionide-fsharp
 .Net: V7.0.202
 
-* El programa ... 
+* El programa calcula todos las posibles rutas desde un punto A a un punto B y elige aquella con menor peso. 
+* Se agregan 2 nuevas funciones al ejercicio visto en clase que se encargan de elegir la ruta con menor peso,
+  estas funciones inician en la línea 60
+* Al inciar, el programa mostrará el grafo, las posibles rutas que se pueden tomar de ir del punto i al punto f, 
+  y la ruta con menor peso. 
 
 Instrucciones para ejecutar el programa: 
-
 
 - Ejecutar con:
 $ dotnet fsi Ejercicio_1.fsx
